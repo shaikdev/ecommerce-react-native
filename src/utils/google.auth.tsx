5947e9083a6f40f6ce253c49e032b7eedc273e8f
Shaik Rahuman <103171089+shaikdev@users.noreply.github.com>
2023-05-29 17:26:04 +0530

    firebase google auth - ios setup

---
+++ src/utils/google.auth.tsx
@@ -0,0 +1,30 @@
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+GoogleSignin.configure({
+  webClientId:
+    '580727078144-3dupj7pdmqghe79v8q58anre33a22tgl.apps.googleusercontent.com',
+});
+
+export const onGoogleButtonPress = async () => {
+  try {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices();
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    const data = await auth().signInWithCredential(googleCredential);
+    return data;
+  } catch (err: any) {
+    console.log(err);
+  }
+};
+
+export const onGoogleSignout = async () => {
+  try {
+    const singnOurt = await auth().signOut();
+  } catch (err: any) {}
+};
